6bfbfb1929f593395f2af2438b313ef696205049
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-01-29 14:47:10 -0600

    update babel-preset-env to @babel/preset-env, update layout and styles, add redirection on click of add recipe button

--- src/index.js
+++ src/index.js
@@ -10,3 +10,14 @@
 //   ],
 //   id: 1234
 // }
+
+import uuidv4 from "uuid/v4"
+
+const addRecipeElement = document.querySelector(".button--add")
+
+addRecipeElement.addEventListener("click", e => {
+  let recipe = {
+    id: uuidv4()
+  }
+  window.location.assign(`./edit.html#${recipe.id}`)
+})
