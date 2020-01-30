632532c70ab33f9f310075dd6904662cda254a77
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-01-29 21:55:38 -0600

    listen for add ingredient click event and start creation of ingredient dom element

--- src/edit.js
+++ src/edit.js
@@ -1,5 +1,28 @@
+import uuidv4 from "uuid/v4"
+
 const titleEl = document.querySelector("#recipe-title")
 const instructionsEl = document.querySelector("#recipe-instructions")
+const ingredientNameEl = document.querySelector("#ingredient-name")
+const addIngredientEl = document.querySelector("#add-ingredient")
+
+let ingredientListEl = document.querySelector("#ingredient-list")
 
 titleEl.addEventListener("input", e => console.log(e.target.value))
 instructionsEl.addEventListener("input", e => console.log(e.target.value))
+// ingredientNameEl.addEventListener("input", e => console.log(e.target.value))
+
+const generateIngredientDOM = ingredient => {
+  const checkbox = document.createElement("input")
+  const label = document.createElement("label")
+  const removeButton = document.createElement("button")
+
+  checkbox.setAttribute("type", "checkbox")
+  
+}
+
+addIngredientEl.addEventListener("click", e => {
+  //current value of ingredientNameEl added to ingredient list in a new dom object
+  console.log('add ingredient')
+  console.log(ingredientNameEl.value)
+  // generateIngredientDOM(ingredientNameEl.value)
+})
