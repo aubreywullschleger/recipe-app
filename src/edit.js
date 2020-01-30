ae698dad6928fa5a91cf0cc0eb1c8c5b3a7ad324
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-01-30 16:25:42 -0600

    generate dom element for each ingredient

--- src/edit.js
+++ src/edit.js
@@ -9,20 +9,53 @@ let ingredientListEl = document.querySelector("#ingredient-list")
 
 titleEl.addEventListener("input", e => console.log(e.target.value))
 instructionsEl.addEventListener("input", e => console.log(e.target.value))
-// ingredientNameEl.addEventListener("input", e => console.log(e.target.value))
 
+// Generate new ingredient element
 const generateIngredientDOM = ingredient => {
+  const listItem = document.createElement("div")
+
+  const ingredientCheckbox = document.createElement("div")
   const checkbox = document.createElement("input")
   const label = document.createElement("label")
+
   const removeButton = document.createElement("button")
 
+  const id = uuidv4()
+
+  // Set up list item
+  listItem.classList.add("list-item")
+
+  // Set up ingredient checkbox
+  ingredientCheckbox.classList.add("list-item__ingredient-checkbox")
+  listItem.appendChild(ingredientCheckbox)
+
+  // Set up checkbox
   checkbox.setAttribute("type", "checkbox")
-  
+  checkbox.id = `${id}`
+  checkbox.name = "ingredient"
+  checkbox.classList.add("list-item__checkbox")  
+  ingredientCheckbox.appendChild(checkbox)
+
+  // Set up label
+  label.setAttribute("for", `${id}`)
+  label.classList.add("list-item__label")
+  label.innerHTML = ingredient
+  ingredientCheckbox.appendChild(label)
+
+  // Set up button
+  removeButton.classList.add("list-item__button")
+  removeButton.innerHTML = "Remove"
+  listItem.appendChild(removeButton)
+
+  return listItem
 }
 
+// Add ingredient event
 addIngredientEl.addEventListener("click", e => {
-  //current value of ingredientNameEl added to ingredient list in a new dom object
-  console.log('add ingredient')
   console.log(ingredientNameEl.value)
-  // generateIngredientDOM(ingredientNameEl.value)
+  if (ingredientNameEl.value.length > 0) {
+    ingredientListEl.appendChild(generateIngredientDOM(ingredientNameEl.value))
+  }
+  ingredientNameEl.value = ""
 })
+//add ingredient button: add another event listener for enter key on keyup, if e.keycode === 13 do something
