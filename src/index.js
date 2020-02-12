3a23da27af45379e0404c6ef3c15c4275408b36c
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-02-12 12:20:12 -0600

    remove unnecessary event handler for clicking on a recipe

--- src/index.js
+++ src/index.js
@@ -11,16 +11,6 @@ document.querySelector("#create-recipe").addEventListener("click", e => {
   window.location.assign(`./edit.html#${id}`)
 })
 
-// Listen for click on recipe item
-document.addEventListener("click", e => {
-  const id = e.target.id
-  const partialIdEnd = id.substring(12)
-
-  if (e.target.classList[0].substring(0,11) === "recipe-item") {
-    window.location.assign(`./edit.html#${partialIdEnd}`)
-  }
-})
-
 // Set up filter with search text
 document.querySelector("#search-text").addEventListener("input", e => {
   setFilters({
